4328439d75bf4b00797585d20624aff97e807c30
AreniumIon <jeffreydickenson9@gmail.com>
2020-08-31 02:15:34 -0500

    Scene Setup

---
+++ Assets/Scripts/HazardVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardVolume : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if collides with player ship, kill it
+        PlayerShip playerShip = other.GetComponent<PlayerShip>();
+        if (playerShip != null)
+            playerShip.Kill();
+    }
+}
